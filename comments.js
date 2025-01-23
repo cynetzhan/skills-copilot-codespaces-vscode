08a48419b4bd6510116782ddcd3c15face8e658e
Hafizh An-Shidqi <hafizhan.shidqi@students.uin-suska.ac.id>
2025-01-23 03:28:02 +0000

    copilot 3rd commit

--- comments.js
+++ comments.js
@@ -1,36 +1,48 @@
-// create web server
+// Create web server using express
+// Load the express module
 const express = require('express');
+// Create an instance of the express module
 const app = express();
+// Load the comments module
+const comments = require('./comments');
+// Load the bodyParser module
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const commentsPath = path.join(__dirname, 'comments.json');
-const comments = require(commentsPath);
+// Load the cors module
+const cors = require('cors');
 
-// middleware
+// Use the cors middleware
+app.use(cors());
+// Use the bodyParser middleware
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
-// routes
-app.get('/api/comments', (req, res) => {
+// Create a GET route that returns the comments
+app.get('/comments', (req, res) => {
   res.json(comments);
 });
 
-app.post('/api/comments', (req, res) => {
-  const newComment = req.body;
-  newComment.id = Date.now();
-  comments.push(newComment);
-
-  fs.writeFile(commentsPath, JSON.stringify(comments, null, 2), (err) => {
-    if (err) {
-      return res.status(500).send('Error writing new comment');
-    }
-
-    res.json(newComment);
-  });
+// Create a POST route that adds a comment
+app.post('/comments', (req, res) => {
+  const { author, text } = req.body;
+  if (author && text) {
+    comments.push({ author, text });
+    res.json({ msg: 'comment added' });
+  } else {
+    res.status(400).json({ msg: 'invalid request' });
+  }
 });
 
-// start the server
-app.listen(3000, () => {
-  console.log('Server is listening on http://localhost:3000');
+// Create a DELETE route that deletes a comment
+app.delete('/comments/:index', (req, res) => {
+  const index = req.params.index;
+  if (comments[index]) {
+    comments.splice(index, 1);
+    res.json({ msg: 'comment deleted' });
+  } else {
+    res.status(404).json({ msg: 'comment not found' });
+  }
+});
+
+// Start the server
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
 });
